193dd19e03e37a54bbb47d7a58f5e7f0d4394a89
Cuchencio <cesarmaureira95@gmail.com>
2022-08-13 20:31:04 -0400

    Challenge beginner día 1

---
+++ Challenge Day 1.py
@@ -0,0 +1,41 @@
+# import sys
+# print(sys.version)
+
+# from datetime import datetime
+# formatoFechas = datetime.today()
+# fecha = formatoFechas.strftime('%d/%m/%y')
+# hora = formatoFechas.strftime('%H:%M:%S')
+# print(fecha, hora)
+
+# import math
+# r = int(input('Ingrese el radio de la circunferencia para calcular el area: '))
+# area = (math.pi) * r**2
+# print(area)
+
+# nombreAlRevez = []
+# nombre = input('Ingresa tu nombre: ')
+# apellido = input('Ingresa tu apellido: ')
+# nombreAlRevez.append(nombre)
+# nombreAlRevez.append(apellido)
+# print(nombreAlRevez[1],nombreAlRevez[0])
+
+# numeros = []
+# while True:
+#     r = int(input('Ingresa el numero que deseas agregar: '))
+#     numeros.append(r)
+#     salida = input('Imprimir lista?: y o n: ')
+#     if salida.lower() == 'y':
+#         print(numeros)
+#         print(tuple(numeros))
+#         break
+
+# fileName = input('Ingresa el nombre del archivo que quieres saber la extención: ')
+# indicePunto = fileName.index('.')
+# print(fileName[indicePunto+1:len(fileName)])
+
+# colorList = ['Rojo', 'Verde', 'Blanco', 'Negro']
+# print(colorList[0], colorList[len(colorList) - 1])
+
+# n = input('Ingrese el numero que desea calcular: ')
+# total = int(n)+int(n+n)+int(n+n+n)
+# print(total)
