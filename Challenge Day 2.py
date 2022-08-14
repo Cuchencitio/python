8899c3a9523d92a9b6d89dfa8352c2c9401dcd1d
Cuchencio <cesarmaureira95@gmail.com>
2022-08-13 21:00:43 -0400

    Avance día 2

---
+++ Challenge Day 2.py
@@ -0,0 +1,13 @@
+# from googletrans import Translator
+# traductor = Translator()
+# frase = input('Ingresa la funcion de la que deseas saber: ')
+# textoTraducido = traductor.translate(frase, dest='es')
+# print(textoTraducido.text)
+
+# funcion = input('Ingresa la funcion que quieres consultar: ')
+# print(help(funcion))
+
+# import calendar
+# mes = input('Ingresa mes en numeros: ')
+# anio = input('Ingresa año: ')
+# print(calendar.month(int(anio), int(mes)))
